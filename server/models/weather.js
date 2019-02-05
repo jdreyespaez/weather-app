b30f7e562872fe5ee1bbdff0b90d52f83a066460
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-05 17:36:12 -0500

    Changing from Imperial to Metric system

--- server/models/weather.js
+++ server/models/weather.js
@@ -5,7 +5,7 @@ var API_KEY = 'c2affa192084647329a0b8a908e0c923';
 class Weather {
     static retrieveByCity (city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`,
             json: true
         }).then(function (res) {
             callback(res);
