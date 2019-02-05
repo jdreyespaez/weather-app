49ac7635d6c0b2de6b0f36b939f24f4ffd59bf91
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-05 16:28:50 -0500

    Configuring the API key and the way to request the data from OWM api

---
+++ server/models/weather.js
@@ -0,0 +1,19 @@
+const request = require('request-promise');
+
+const API_KEY = '8923642cc6a019b215609d37851e0e9d';
+
+class Weather {
+    static retrieveByCity (city, callback) {
+        request({
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_key}&units=imperial`,
+            json: true
+        }).then(function (res) {
+            callback(res);
+        }).catch(function (err) {
+            console.log(err);
+            callback({ error: 'Could not reach OWM Api.' });
+        })
+    }
+}
+
+module.export = Weather;
