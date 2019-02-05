0d83ea1224d942317f570fc30da7d1b69405f8da
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-05 16:55:56 -0500

    API_KEY was not defined, because the const was API_KEY and it was used as API_key

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,11 +1,11 @@
 const request = require('request-promise');
 
-const API_KEY = '8923642cc6a019b215609d37851e0e9d';
+var API_KEY = 'c2affa192084647329a0b8a908e0c923';
 
 class Weather {
     static retrieveByCity (city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_key}&units=imperial`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
             json: true
         }).then(function (res) {
             callback(res);
