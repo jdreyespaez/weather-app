5c6078ade0b49724b07b55e875683f8b574cf632
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-05 15:40:50 -0500

    Adding the Router the API, first the get method to retrieveAll the data

---
+++ server/api/cities.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var Cities = require('../models/cities');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    Cities.retrieveAll(function (err, cities) {
+        if (err)
+            return res.json(err);
+        return res.json(cities);
+    });
+});
+
+router.post('/', function (req, res) {
+
+});
