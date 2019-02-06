acfd83294c29412207d0bf3d5200d6b4c9d44864
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-05 19:40:32 -0500

    Importing components necessary

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
+import { 
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+  Col
+} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Container fluid className="centered">
+        <Navbar dark color="dark">
+          <NavbarBrand href="/">MyWeather</NavbarBrand>
+        </Navbar>
+        <Row>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
     );
   }
 }
