1f8158fdbbf97eb5bca8b71994886af4e324d8a8
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-07 09:23:50 -0500

    Fetching name, img icon, main temp

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -9,10 +9,10 @@ const Weather = (props) => {
     return (
         <Row className="weather">
             <Col sm="12" md={{ size: 4, offset: 4 }}>
-            <h2></h2>
-            <img />
-            <span></span>
-            <span></span>
+            <h2>{data.name}</h2>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png)`} alt="Weather Icon"/>
+            <span>{data.weather[0].main}</span>&nbsp;
+            <span>{Math.floor(data.main.temp)}&deg;C</span>
             <Table>
                 <tbody></tbody>
             </Table>
