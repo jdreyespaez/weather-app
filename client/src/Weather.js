876330913ded3fd6a3bc98ccc6cd787bbd50212c
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2019-02-07 09:36:49 -0500

    Importing data from the OWM api

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -10,11 +10,32 @@ const Weather = (props) => {
         <Row className="weather">
             <Col sm="12" md={{ size: 4, offset: 4 }}>
             <h2>{data.name}</h2>
-            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png)`} alt="Weather Icon"/>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} alt="Weather Icon"/>
             <span>{data.weather[0].main}</span>&nbsp;
             <span>{Math.floor(data.main.temp)}&deg;C</span>
             <Table>
-                <tbody></tbody>
+                <tbody>
+                    <tr>
+                        <td>Wind</td>
+                        <td>{Math.floor(data.wind.speed)} km/h</td>
+                    </tr> 
+                    <tr>
+                        <td>Pressure</td>
+                        <td>{Math.floor(data.main.pressure)} hPa</td>
+                    </tr>    
+                    <tr>
+                        <td>Humidity</td>
+                        <td>{Math.floor(data.main.humidity)} %</td>
+                    </tr> 
+                    <tr>
+                        <td>Min Temp</td>
+                        <td>{Math.floor(data.main.temp_min)} &deg;C</td>
+                    </tr> 
+                    <tr>
+                        <td>Max Temp</td>
+                        <td>{Math.floor(data.main.temp_max)} &deg;C</td>
+                    </tr> 
+                </tbody>
             </Table>
             </Col>
         </Row>
